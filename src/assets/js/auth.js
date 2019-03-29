c8fbbacaca183f1fabd49ce633fd1cf70d30f6af
faog <fabiola.orellana.g@gmail.com>
2019-03-29 18:20:50 -0300

    inicialización firebase con script y variable config

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,4 +1,16 @@
 //Acá no hay manejo del DOM
+//Inicialización de firebase
+
+var config = {
+    apiKey: "AIzaSyCVDZrZyWXI-xAVlz9mCDjLS48i1DKFTWU",
+    authDomain: "pet-community-sn.firebaseapp.com",
+    databaseURL: "https://pet-community-sn.firebaseio.com",
+    projectId: "pet-community-sn",
+    storageBucket: "pet-community-sn.appspot.com",
+    messagingSenderId: "443792892793"
+    };
+  
+  firebase.initializeApp(config);
 
 /*
 1-Crear 2 funciones que simularan el login con google y la creación de cuenta 
