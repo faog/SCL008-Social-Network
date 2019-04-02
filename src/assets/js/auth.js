b0eb1fb847b534031e68fac845ff95b266e0ff4f
faog <fabiola.orellana.g@gmail.com>
2019-04-02 17:31:00 -0300

    configuración test, usando jest

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,19 +1,4 @@
-//Acá no hay manejo del DOM
-//Inicialización de firebase
-
-var config = {
-  apiKey: "AIzaSyCVDZrZyWXI-xAVlz9mCDjLS48i1DKFTWU",
-  authDomain: "pet-community-sn.firebaseapp.com",
-  databaseURL: "https://pet-community-sn.firebaseio.com",
-  projectId: "pet-community-sn",
-  storageBucket: "pet-community-sn.appspot.com",
-  messagingSenderId: "443792892793"
-};
-
-firebase.initializeApp(config);
-
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+import {firebase} from "./firebase.js"
 
 export const loginGoogle = () =>{
   return 'Login con Google Ok';
@@ -22,64 +7,60 @@ export const loginGoogle = () =>{
 /*Función para crear una nueva cuenta*/
 export const createAccount = () =>{
   cleanErrors();
-  let emailNewUser = document.getElementById('emailnewuser').value;
-  let passwordNewUser = document.getElementById('passwordnewuser').value;
   let firstNameNewUser = document.getElementById('firstnamenewuser').value;
   let lastNameNewUser = document.getElementById('lastnamenewuser').value;
+  let emailNewUser = document.getElementById('emailnewuser').value;
+  let passwordNewUser = document.getElementById('passwordnewuser').value;
   let valid = true;
   
+  if (firstNameNewUser===""){
+    document.getElementById('firstnameerror').innerHTML=`*Debes ingresar un nombre.`;
+  }
+
+  if (lastNameNewUser===""){
+    document.getElementById('lastnameerror').innerHTML=`*Debes ingresar un apellido.`;
+  } 
+
   if(emailNewUser===""){
     document.getElementById('emailerror').innerHTML=`*Debes ingresar un correo válido.`;
   }
-  if (firstNameNewUser===""){
-    document.getElementById('emailerror').innerHTML=`*Debes ingresar un nombre.`;
-  }
-  if (lastNameNewUser===""){
-    document.getElementById('emailerror').innerHTML=`*Debes ingresar un apellido.`;
-  } 
 
-  if (passwordNewUser===""){
-    document.getElementById('emailerror').innerHTML=`*Debes ingresar una contraseña.`;
-  } 
+  if(passwordNewUser===""){
+    document.getElementById('passworderror').innerHTML=`*Debes ingresar una contraseña con minimo 6 caracteres.`;
+  }
 
   if(!valid){
     return;
   }
 
+  if(firstNameNewUser === ""|| lastNameNewUser ==="" || emailNewUser==="" || passwordNewUser===""){
+    console.log("usuario no creado");
+  }else{
+    db.collection("users").add({
+      firstname:firstNameNewUser,
+      lastname:lastNameNewUser,
+      email:emailNewUser,
+      password: passwordNewUser
+    })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });
+  }
+
+
   /*Función de Firebase para registrar nuevos usuarios*/
   firebase.auth().createUserWithEmailAndPassword(emailNewUser, passwordNewUser)
   .catch(function(error) {
-    // Handle Errors here.
-    /*if(error){
-      if(error.code==="auth/invalid-email"){
-        document.getElementById('emailerror').innerHTML=`*Debes ingresar un correo válido.`;
-      }
-      else if (error.code==="auth/weak-password"){
-        document.getElementById('passworderror').innerHTML=`*La contraseña debe ser superior a 5 carácteres.`;
-      }
-      else if (error.code==="auth/email-already-in-use"){
-        document.getElementById('emailerror').innerHTML=`*Este correo ya se encuentra registrado.`;
-      }
-      else{
-        alert('No se ha podido crear el usuario');
-      }
-    }*/
-    // Add a new document in collection "cities"
-    db.collection("users").add({
-      firstname: firstNameNewUser,
-      lastname: lastNameNewUser,
-      email: emailNewUser,
-      password:passwordNewUser  
-  })
-  .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });
+    if(error){
+      alert('No se ha podido crear el usuario');
+    }
+    
+  }
+)}
 
-  });
-}
 
 function cleanErrors(){
   document.getElementById('emailerror').innerHTML='';
@@ -89,3 +70,8 @@ function cleanErrors(){
 }
 
 //Este archivo no lleva nada mas, por lo que se debe testear
+
+//Función de ejemplo
+export const suma =(a,b)=>{   
+  return a+b;
+}
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,1 +1,16 @@
 //Test de autentificación firebase
+/*TEST DE EJEMPLO*/
+import {suma, createAccount } from "./../src/assets/js/auth.js"
+
+describe ("suma", ()=>{
+    it ('deberia retornar 4 para la suma de 2 mas 2', ()=>{
+        expect(suma(2,2)).toBe(4);
+    })
+
+})
+
+describe ("createAccount",()=>{
+    it('deberia retornar Debes ingresar un nombre si el usuario deja el input vacío', ()=>{
+        expect(createAccount(firstNameNewUser,"").toBe("*Debes ingresar un nombre"));
+    })
+})
