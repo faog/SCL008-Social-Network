21639a8e392c83044ab59b7ad6706e559c2d74fe
faog <fabiola.orellana.g@gmail.com>
2019-04-03 19:35:13 -0300

    Validación de la función validateAccount para usar en createAccount

--- src/assets/js/validation.js
+++ src/assets/js/validation.js
@@ -1,25 +1,8 @@
 export const validateAccount = (firstNameNewUser,lastNameNewUser,emailNewUser,passwordNewUser) => {
-  
-  if (document.getElementById('firstnameerror') && firstNameNewUser===""){
-    document.getElementById('firstnameerror').innerHTML=`*Debes ingresar un nombre.`;
-  }
-
-  if (document.getElementById('lastnameerror') && lastNameNewUser===""){
-    document.getElementById('lastnameerror').innerHTML=`*Debes ingresar un apellido.`;
-  } 
-
-  if(document.getElementById('emailerror') && emailNewUser===""){
-    document.getElementById('emailerror').innerHTML=`*Debes ingresar un correo válido.`;
-  }
-
-  if(document.getElementById('passworderror') && passwordNewUser===""){
-    document.getElementById('passworderror').innerHTML=`*Debes ingresar una contraseña con minimo 6 caracteres.`;
-  }
-
   if(firstNameNewUser === ""|| lastNameNewUser ==="" || emailNewUser==="" || passwordNewUser===""){
     return false;
   }else{
-      return true;
+    return true;
   }
 }
 
