dd99ef5e35aa26c2149fa1ddef541c10cf045225
faog <fabiola.orellana.g@gmail.com>
2019-04-05 10:46:49 -0300

    Evento que obtiene los datos del usuario para traspasarlos al archivo validation.js y validaciones de excritura en el DOM

--- src/assets/views/templateSignIn.js
+++ src/assets/views/templateSignIn.js
@@ -1,14 +1,44 @@
+import {signIn} from "./../js/auth.js";
+
 export const templateSignIn = () =>{
     /*Obtengo el id desde el html donde quiero que se imprima mi ventana inicial y le traspaso el 
     contenido a imprimir*/
+    
+    
     document.getElementById('containersocialnetwork').innerHTML=    
         `
-        <input type="email" id="email" placeholder="ingrese su correo"/>
-        <input type="password" id="password" placeholder="ingrese su contraseña"/>
+        <input type="email" id="emailsignin" placeholder="ingrese su correo"/>
+        <p id="emailerror" class=""></p>
+        <input type="password" id="passwordsigin" placeholder="ingrese su contraseña"/>
+        <p id="passworderror" class=""></p>
         <button id="signin">Iniciar Sesión</button>   
         <a href="#/login">Google</a>    
         <a href="#/create">Crear cuenta</a>  
-        `;   
+        `;      
+    
+    document.getElementById('signin').addEventListener('click', () => {
+        let emailSignIn = document.getElementById('emailsignin').value;
+        let passwordSignIn = document.getElementById('passwordsigin').value;
+        let userSignIn = signIn(emailSignIn,passwordSignIn);
 
+        /*IMPRESION VÁLIDACIONES EN EL DOM*/
+        if(emailSignIn===""){
+            document.getElementById('emailerror').innerHTML=`*Debes ingresar un correo válido.`;
+        }else{
+            document.getElementById('emailerror').innerHTML='';
+        }
+        
+        if(passwordSignIn===""){
+            document.getElementById('passworderror').innerHTML=`*Debes ingresar una contraseña con minimo 6 caracteres.`;
+        }else{
+            document.getElementById('passworderror').innerHTML='';
+        }
+
+        if(userSignIn==="OK"){
+            
+        } else {
+            console.log(userSignIn);
+        }
+    })
     
 }
