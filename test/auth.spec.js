bf575cd558171332f7f8811d57133a1bead33ce3
faog <fabiola.orellana.g@gmail.com>
2019-04-05 10:54:09 -0300

    Creación de test para las válidaciones validateSignIn() y validateEmailSignIn()

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,4 +1,6 @@
-import {validateAccount,validateEmail} from "./../src/assets/js/validation.js"
+import {validateAccount,validateEmail, validateSignIn,validateEmailSignIn} 
+from "./../src/assets/js/validation.js"
+
 
 /*1) Validaciones de las funciones que chequean createAccount()*/
 describe("validateAccount",()=>{
@@ -32,4 +34,29 @@ describe("validateEmail",()=>{
     })
 })
 
-/*2) */
+/*2) Validaciones de las funciones que chequean signIn() */
+describe("validateSignIn",()=>{
+    it('deberia retornar true, si el usuario ingresa todos los campos',()=>{
+        expect(validateSignIn("fabiola.orellana.g@gmail.com","clavecita"))
+        .toBe(true);
+    })
+})
+
+describe("validateSignIn",()=>{
+    it('deberia retornar false, si el usuario no ingresa ningún cambio',()=>{
+        expect(validateSignIn("",""))
+        .toBe(false);
+    })
+})
+
+describe("validateEmailSignIn",()=>{
+    it('deberia retornar false, si el correo es invalido',()=>{
+        expect(validateEmailSignIn("correomalo")).toBe(false);
+    })
+})
+
+describe("validateEmailSignIn",()=>{
+    it('deberia retornar true, si el correo es valido',()=>{
+        expect(validateEmailSignIn("correobueno@gmail.com")).toBe(true);
+    })
+})
