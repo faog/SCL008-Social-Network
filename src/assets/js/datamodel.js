affffa9859d37f5575a9321712175f9a0e781f46
faog <fabiola.orellana.g@gmail.com>
2019-04-08 17:43:00 -0400

    comentarios funcion postCreate

--- src/assets/js/datamodel.js
+++ src/assets/js/datamodel.js
@@ -1,14 +1,14 @@
 //Variable que obtiene la inicialización de firestone 
 let dbPost = firebase.firestore();
 
-
-
+/*1.)Función para crear un nuevo post
+Necesito traspasar desde el template el texto de la publicación*/
 export const postCreate = (textPost) =>{
     let date=Date.now();
-  
-    dbPost.collection("post").add({
-        text:textPost,
+
+    dbPost.collection("post").add({        
         user:firebase.auth().currentUser.email,
+        text:textPost,
         date:date
     })
     .then(function(docRef) {
