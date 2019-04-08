d565c083a7423f8adcd36a7bea49b72f4ba3628f
faog <fabiola.orellana.g@gmail.com>
2019-04-08 16:12:43 -0400

    html básico para esta vista y creación del evento postCreate(textPost)

--- src/assets/views/templateTimeLine.js
+++ src/assets/views/templateTimeLine.js
@@ -1,17 +1,30 @@
-import { signOut } from "./../js/auth.js";
+import {signOut} from "./../js/auth.js";
+import {postCreate} from "./../js/datamodel.js"
 
 export const templateTimeLine = () =>{
-    const containerLogin = document.getElementById('containersocialnetwork');
-    
-    //Crearemos el contenido del login
-    const contentLogin =`
-                        <p>Muro</p>  
-                        <button id="signout">Salir</button>                      
+    document.getElementById('containersocialnetwork').innerHTML=
+                        `
+                        <section id="userinformation">
+                            <h3>Muro</h3> 
+                            <p>Usuario1</p>
+                        </section>
+                        <section id="postinformation">
+                            <textarea id="textpost" placeholder="¿Qué estás pensando?"></textarea>
+                            <button id="post">Publicar</button>
+                            <button id="signout">Salir</button>  
+                        </section>
+                        <section id="timeline">
+                            
+                        </section>                    
                         `;
     
-    //traspasar el contenido de contentLogin al div
-    containerLogin.innerHTML = contentLogin;
+    //evento para publicar un mensaje   
+    document.getElementById('post').addEventListener('click',()=>{
+        let textPost = document.getElementById('textpost').value;
+        postCreate(textPost);
+    })
 
+    //evento para cerrar la sesión del usuario
     document.getElementById('signout').addEventListener('click', ()=>{
         signOut();
     })
