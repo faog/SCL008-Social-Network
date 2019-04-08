338a1b834a943d04895f653ab2887776326c5258
majomarquez <majomarquez@gmail.com>
2019-04-08 17:27:38 -0400

    funcion de post para muro

--- src/assets/js/datamodel.js
+++ src/assets/js/datamodel.js
@@ -1,3 +1,21 @@
 //Variable que obtiene la inicialización de firestone 
 let dbPost = firebase.firestore();
 
+
+
+export const postCreate = (textPost) =>{
+    let date=Date.now();
+  
+    dbPost.collection("post").add({
+        text:textPost,
+        user:firebase.auth().currentUser.email,
+        date:date
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        document.getElementById("textpost").value="";
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
