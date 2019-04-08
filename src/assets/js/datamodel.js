d0f9ba57ded3045e920029fe9e8211c47ea4a622
faog <fabiola.orellana.g@gmail.com>
2019-04-08 18:03:07 -0400

    comentarios función postCreate

--- src/assets/js/datamodel.js
+++ src/assets/js/datamodel.js
@@ -6,14 +6,14 @@ Necesito traspasar desde el template el texto de la publicación*/
 export const postCreate = (textPost) =>{
     let date=Date.now();
 
-    dbPost.collection("post").add({        
-        user:firebase.auth().currentUser.email,
-        text:textPost,
-        date:date
+    dbPost.collection("post").add({
+        user: firebase.auth().currentUser.email,
+        message: textPost,
+        date:date         
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
-        document.getElementById("textpost").value="";
+        document.getElementById("textpost").value='';  
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
