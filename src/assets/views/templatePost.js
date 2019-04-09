0c36096005f4b35b2204b693e39b0373f8c4de78
faog <fabiola.orellana.g@gmail.com>
2019-04-09 10:40:42 -0400

    Template que permite al usuario ingresar su post, e interactua con el datamodel.js

--- src/assets/views/templatePost.js
+++ src/assets/views/templatePost.js
@@ -1,4 +1,5 @@
 import { observer } from "./../js/auth.js";
+import {postCreate} from "./../js/datamodel.js"
 
 export const templatePost =()=>{
     observer();
@@ -11,7 +12,13 @@ export const templatePost =()=>{
                             <img src="" id="userphoto" alt="Foto del usuario"/>
                             <h2>${firebase.auth().currentUser.displayName}</h2>                                                                                
                         </section>
-
+                        <textarea id="textpost" placeholder="¿Qué estás pensando?"></textarea>   
                         <button id="post">Publicar</button>  
                         `
+
+    //evento para publicar un mensaje   
+    document.getElementById('post').addEventListener('click',()=>{
+        let textPost = document.getElementById('textpost').value;
+        postCreate(textPost);
+    })                    
 }
