1b5bb0cc3e00e3b5e1bdd491e87a426e1ed497bf
faog <fabiola.orellana.g@gmail.com>
2019-04-08 22:12:05 -0400

    Actualización vista

--- src/assets/views/templateTimeLine.js
+++ src/assets/views/templateTimeLine.js
@@ -1,5 +1,6 @@
 import {signOut} from "./../js/auth.js";
 import {postCreate} from "./../js/datamodel.js"
+import { templatePost } from "./templatePost.js";
 
 export const templateTimeLine = () =>{
     document.getElementById('containersocialnetwork').innerHTML=
@@ -9,12 +10,36 @@ export const templateTimeLine = () =>{
                             <p>Usuario1</p>
                             <button id="signout">Salir</button> 
                         </section>
-                        <section id="postinformation">
-                            <textarea id="textpost" placeholder="¿Qué estás pensando?"></textarea>
-                            <button id="post">Publicar</button>                            
+                        <section id="postinformation">                            
+                            <img src="${firebase.auth().currentUser.photoURL}" id="userphoto" alt="Foto del usuario"/>
+                            <textarea id="textpost" placeholder="¿Qué estás pensando?"></textarea>                                                      
                         </section>
+                        <button id="post">Publicar</button>  
                         <section id="timeline">
-                            
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
+                            <h1>Hola2</h1>
                         </section>                    
                         `;
     
@@ -24,6 +49,9 @@ export const templateTimeLine = () =>{
         postCreate(textPost);
     })
 
+    document.getElementById('textpost').addEventListener('click',()=>{
+        templatePost();
+    })
     //evento para cerrar la sesión del usuario
     document.getElementById('signout').addEventListener('click', ()=>{
         signOut();
