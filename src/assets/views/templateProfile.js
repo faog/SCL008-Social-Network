8902a9f5a6483bfcdb4be91b1c19669f946d0a96
faog <fabiola.orellana.g@gmail.com>
2019-04-09 12:09:23 -0400

    Se agrega boton para volver al templateTimeLine

--- src/assets/views/templateProfile.js
+++ src/assets/views/templateProfile.js
@@ -4,8 +4,13 @@ export const templateProfile =()=>{
     observer();
     document.getElementById('containersocialnetwork').innerHTML=
                         `
+                        <nav id="navinformation">
+                            <a href="#/timeline" class="brand-logo">
+                                <img src="assets/Images/left-arrow.png" id="backtimeline" alt="volver al muro"/>                            
+                            </a>  
+                        </nav>
                         <section id="userinformation">
-                            <h3>salir</h3> 
+                            <h3>perfil</h3> 
                         </section>
                         `
 }
