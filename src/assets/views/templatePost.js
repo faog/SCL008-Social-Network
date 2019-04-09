78e0f84f6901562cc3d7cd461837ed90d52c8afa
faog <fabiola.orellana.g@gmail.com>
2019-04-09 12:08:41 -0400

    Se incluye nuevo html para esta vista

--- src/assets/views/templatePost.js
+++ src/assets/views/templatePost.js
@@ -1,21 +1,22 @@
-import { observer } from "./../js/auth.js";
+import {observer} from "./../js/auth.js";
 import {postCreate} from "./../js/datamodel.js"
 
 export const templatePost =()=>{
     observer();
     document.getElementById('containersocialnetwork').innerHTML=
                         `
-                        <section id="userinformation">
-                            <h3>salir</h3> 
-                        </section>
+                        <nav id="navinformation">
+                            <a href="#/timeline" class="brand-logo">
+                                <img src="assets/Images/left-arrow.png" id="backtimeline" alt="volver al muro"/>                            
+                            </a>  
+                        </nav>
                         <section id="userinformation">                            
-                            <img src="" id="userphoto" alt="Foto del usuario"/>
-                            <h2>${firebase.auth().currentUser.displayName}</h2>                                                                                
+                            <img src=""id="userphoto" alt="Foto del usuario"/>
+                            <h2 id="username">${firebase.auth().currentUser.displayName}</h2>                                                                                
                         </section>
-                        <textarea id="textpost" placeholder="¿Qué estás pensando?"></textarea>   
+                        <textarea id="textpost" placeholder="¿Qué estás pensando?" autofocus></textarea>   
                         <button id="post">Publicar</button>  
                         `
-
     //evento para publicar un mensaje   
     document.getElementById('post').addEventListener('click',()=>{
         let textPost = document.getElementById('textpost').value;
